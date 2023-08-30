a70202e207e4d36a5c7f290e73693478a67dfc49
Nhaykwollfer <108698094+Nhaykwollfer@users.noreply.github.com>
2023-08-30 10:06:16 -0300

    Alterações em andamento

--- labeddit/src/App.js
+++ labeddit/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import Router from './routes/Router';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+
+  html {
+    font-family: 'IBM Plex Sans', sans-serif;
+  }
+
+  button, input {
+    font-family: 'Noto Sans', sans-serif;
+  }
+
+  #root {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #E5E5E5;
+  }
+`
+
+export const AppContainer = styled.div`
+  background-color: #FAFAFA;
+  width: 428px;
+  max-width: 100vw;
+  height: 926px;
+  max-height: 100vh;
+`
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <GlobalStyle />
+      <Router />
+    </AppContainer>
+  )
 }
-
-export default App;
---
+++ labeddit/src/constants/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://labeddit-backend-lpc.onrender.com"
+export const TOKEN_NAME = "labeddit"
